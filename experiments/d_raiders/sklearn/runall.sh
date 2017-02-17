46e2afb8ed6de16b170e85e6cc0622953fad7e63
Edward Gan <edgan8@gmail.com>
2017-02-17 13:47:31 -0800

    bug in d_scale python runner

--- experiments/d_raiders/sklearn/runall.sh
+++ experiments/d_raiders/sklearn/runall.sh
@@ -7,5 +7,5 @@ D=experiments/d_raiders/sklearn
 for i in 1 2 4 8 16 32 64 128 256
 do
     echo "Running n $i"
-    python3 $d/mnist_${1}.py | tee $D/out/mnist_${i}.out
+    python3 $D/mnist_${i}.py | tee $D/out/mnist_${i}.out
 done
