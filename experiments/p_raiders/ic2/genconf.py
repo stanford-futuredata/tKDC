f76561865d3c797b24d45c2aeded24fe8958923c
Edward Gan <edgan8@gmail.com>
2017-02-17 17:38:55 -0800

    fixed by with p level test

--- experiments/p_raiders/ic2/genconf.py
+++ experiments/p_raiders/ic2/genconf.py
@@ -1,14 +1,14 @@
-ps = [1,2,3,4,5,10,20,30,40,50,60,70,80,90,95,99]
+ps = [1,5,10,20,30,40,50,60,70,80,90,95,99]
 for p in ps:
     output = """
 inputPath: "bigdata/otmy3.csv"
 inputColumnRange: 0-3
 inputRows: 0
-timeToScore: 30.0
+timeToScore: 60.0
 
 tKDEConf:
   algorithm: TREEKDE
-  percentile: 0.01
+  percentile: {p}
 
   qSampleSize: 20000
   qCutoffMultiplier: 1.1
