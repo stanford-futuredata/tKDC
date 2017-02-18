359c36a129079406872e259b4f3a1916fb36b0b3
Edward Gan <edgan8@gmail.com>
2017-02-17 18:49:26 -0800

    updated plevel tests

--- experiments/p_raiders/simple/genconf.py
+++ experiments/p_raiders/simple/genconf.py
@@ -1,9 +1,5 @@
 ps = [1,5,10,20,30,40,50,60,70,80,90,95,99]
 for p in ps:
-    if p > 60:
-        qs = 4000
-    else:
-        qs = 20000
     output = """
 inputPath: "bigdata/otmy3.csv"
 inputColumnRange: 0-3
@@ -11,12 +7,8 @@ inputRows: 0
 timeToScore: 60.0
 
 tKDEConf:
-  algorithm: TREEKDE
+  algorithm: SIMPLEKDE
   percentile: {p}
-
-  qSampleSize: {qs}
-  qCutoffMultiplier: 1.1
-  qTolMultiplier: 0.01
 
   kernel: gaussian
   denormalized: false
@@ -24,14 +16,12 @@ tKDEConf:
   useStdDev: true
   ignoreSelfScoring: true
 
-  calculateCutoffs: true
+  calculateCutoffs: false
 
   leafSize: 20
   splitByWidth: true
 
-  useGrid: true
-  gridSizes:
-    - 0.8
-""".format(p=0.01 * p, qs=qs)
+  useGrid: false
+""".format(p=0.01 * p)
     with open("tmy3_{}.yaml".format(p), 'w') as f:
         f.write(output)
---
+++ experiments/p_raiders/simple/runall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+D=experiments/p_raiders/simple
+
+for p in 1 5 10 20 30 40 50 60 70 80 90 95 99
+do
+    echo "Running p $p"
+    ./run2.sh $D/tmy3_${p}.yaml | tee $D/out/tmy3_${p}.out
+done
