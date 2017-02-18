04cc78c743891404e9577ea74a4978c018a53a78
Edward Gan <edgan8@gmail.com>
2017-02-18 01:04:09 -0800

    scale p results

--- experiments/p_raiders/ic2/runall.sh
+++ experiments/p_raiders/ic2/runall.sh
@@ -4,7 +4,8 @@ set -o pipefail
 
 D=experiments/p_raiders/ic2
 
-for p in 1 5 10 20 30 40 50 60 70 80 90 95 99
+# for p in 1 5 10 20 30 40 50 60 70 80 90 95 99
+for p in 70 80 90 95 99
 do
     echo "Running p $p"
     ./run2.sh $D/tmy3_${p}.yaml | tee $D/out/tmy3_${p}.out
