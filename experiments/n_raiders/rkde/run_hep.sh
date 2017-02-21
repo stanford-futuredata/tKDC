15be40eb106474af4b2834570c7f1fcafa83bf20
Edward Gan <edgan8@gmail.com>
2017-02-21 12:40:31 -0800

    rkde hep n

---
+++ experiments/n_raiders/rkde/run_hep.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+D=experiments/n_raiders/rkde
+
+for i in 0 1 2 3 4 5 6 7 8 9 10
+do
+    echo "Running n $i"
+    ./run2.sh $D/hep_n${i}.yaml | tee $D/out/hep_n${i}.txt
+done
