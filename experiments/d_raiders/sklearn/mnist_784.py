847c725d73d691f61fd87bd88fb05e1f517031ce
Edward Gan <edgan8@gmail.com>
2017-02-20 23:05:13 -0800

    python2 compatibility

---
+++ experiments/d_raiders/sklearn/mnist_784.py
@@ -0,0 +1,13 @@
+
+import run_kde
+
+run_kde.run_benchmark(
+    df_path="bigdata/mnist.csv",
+    n=70000,
+    numScore=1000,
+    tol=0.01,
+    cols=list(range(1, 785)),
+    bwValue=1000.0,
+    denorm=True,
+)
+
